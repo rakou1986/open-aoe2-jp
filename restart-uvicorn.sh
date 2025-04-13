82004fdf09ec1d6ac14bf1a758bd98be84f919c1
rakou1986 <rakou1986@gmail.com>
2025-04-14 08:06:38 +0900

    uvicornを再起動する。crontabでacme.sh --renewのあとacme.sh --install-certして、そのあとで使う

---
+++ restart-uvicorn.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+cd ~/open-aoe2-jp/ || exit 1
+
+pkill -f "uvicorn main:app"
+sleep 3
+nohup uvicorn main:app \
+    --host 0.0.0.0 \
+    --port 8443 \
+    --ssl-keyfile ~/open-aoe2-jp/certs/open-aoe2-jp.key \
+    --ssl-certfile ~/open-aoe2-jp/certs/open-aoe2-jp.fullchain.pem > ~/uvicorn.log 2>&1 &
