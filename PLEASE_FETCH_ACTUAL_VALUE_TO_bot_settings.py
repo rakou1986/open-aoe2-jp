f8283eb0ed02303000fbbf2f8cdf8cb466b7388c
rakou1986 <rakou1986@gmail.com>
2025-08-07 03:24:07 +0900

    リポジトリにbot_settings.pyが入ってないと謎なので、ひな形を追加

---
+++ PLEASE_FETCH_ACTUAL_VALUE_TO_bot_settings.py
@@ -0,0 +1,16 @@
+# これらはDiscord開発者モードをONにして、サーバー、チャンネル、ユーザーを右クリックして取得する
+canary_bot_status_channel_id = int
+canary_bot_target_channel_id = int
+canary_bot_server_id = int
+canary_bot_info_channel_id = canary_bot_target_channel_id
+canary_bot_authorized_user_ids = [
+    int,
+    ]
+
+available_bot_status_channel_id = int
+available_bot_target_channel_id = int
+available_bot_info_channel_id = int
+available_bot_server_id = int
+available_bot_authorized_user_ids = [
+    int,
+    ]
